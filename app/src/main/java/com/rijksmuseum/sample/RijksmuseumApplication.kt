3f95f0f5359e75f17a80dc5e3480be3095cc5bea
Nicolas Hurtado <nicolashleon@users.noreply.github.com>
2019-08-12 17:58:20 -0500

    Added cache interceptor to facilitate modify the response headers and allow the app to keep the responses from the server for 5 mins including offline

--- app/src/main/java/com/rijksmuseum/sample/RijksmuseumApplication.kt
+++ app/src/main/java/com/rijksmuseum/sample/RijksmuseumApplication.kt
@@ -2,6 +2,7 @@ package com.rijksmuseum.sample
 
 import android.app.Application
 import com.rijksmuseum.sample.network.interceptors.APIAuthInterceptor
+import com.rijksmuseum.sample.network.interceptors.CacheInterceptor
 import com.rijksmuseum.sample.network.interceptors.FormatQueryInterceptor
 import com.rijksmuseum.sample.network.services.CollectionService
 import com.rijksmuseum.sample.network.services.EventService
@@ -11,6 +12,7 @@ import com.rijksmuseum.sample.ui.viewmodels.CollectionViewModel
 import com.rijksmuseum.sample.ui.viewmodels.EventListViewModel
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,6 +22,7 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.File
 import java.util.*
 
 
@@ -28,9 +31,16 @@ class RijksmuseumApplication : Application() {
     private val appModule = module {
 
         single {
+
+            //setup cache
+            val httpCacheDirectory = File(this@RijksmuseumApplication.cacheDir, "responses")
+            val cacheSize = 10 * 1024 * 1024L // 10MB
+            val cache = Cache(httpCacheDirectory, cacheSize)
             val builder = OkHttpClient.Builder()
-                .addInterceptor(FormatQueryInterceptor())
-                .addInterceptor(APIAuthInterceptor())
+                    .cache(cache)
+                    .addInterceptor(FormatQueryInterceptor())
+                    .addInterceptor(APIAuthInterceptor())
+                    .addNetworkInterceptor(CacheInterceptor(this@RijksmuseumApplication))
             if (BuildConfig.DEBUG) {
                 builder.addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC })
             }
@@ -38,31 +48,27 @@ class RijksmuseumApplication : Application() {
         }
         single {
             Moshi.Builder()
-                .add(Date::class.java, Rfc3339DateJsonAdapter())
-                .build()
+                    .add(Date::class.java, Rfc3339DateJsonAdapter())
+                    .build()
         }
         single {
             Retrofit.Builder()
-                .client(get())
-                .addConverterFactory(MoshiConverterFactory.create(get()))
-                .baseUrl(BuildConfig.API_URL)
-                .build()
+                    .client(get())
+                    .addConverterFactory(MoshiConverterFactory.create(get()))
+                    .baseUrl(BuildConfig.API_URL)
+                    .build()
         }
         single {
             val retrofit: Retrofit = get()
             retrofit.create(EventService::class.java)
         }
-
         single {
             val retrofit: Retrofit = get()
             retrofit.create(CollectionService::class.java)
         }
-
         single {
             EventRepository(get())
         }
-
-
         single {
             CollectionRepository(get())
         }
