c39c3f47b4a5428f85c90832b49c7cb59161afd4
Nicolas Hurtado <nicolashleon@users.noreply.github.com>
2019-08-12 20:43:01 -0500

    Mix the state and the events to have a single source of truth as recommended by the Android guidelines

--- app/src/main/java/com/rijksmuseum/sample/ui/viewmodels/CollectionViewModel.kt
+++ app/src/main/java/com/rijksmuseum/sample/ui/viewmodels/CollectionViewModel.kt
@@ -3,27 +3,29 @@ package com.rijksmuseum.sample.ui.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.rijksmuseum.sample.R
 import com.rijksmuseum.sample.repositories.CollectionRepository
 import com.rijksmuseum.sample.ui.Result
 import com.rijksmuseum.sample.ui.models.DelegateUIModel
+import com.rijksmuseum.sample.ui.models.EmptyStateItem
 import kotlinx.coroutines.launch
 
 class CollectionViewModel(private val collectionRepository: CollectionRepository) : ViewModel() {
 
     val collectionItems = MutableLiveData<Result<List<DelegateUIModel>>>()
-    val loader = MutableLiveData<Boolean>()
 
     fun getCollection(maker: String) {
         viewModelScope.launch {
-            loader.postValue(true)
+            collectionItems.postValue(Result.Loading())
             try {
-                val collectionItemList = collectionRepository.getCollection(maker)
-                collectionItems.postValue(Result(collectionItemList))
+                var collectionItemList: List<DelegateUIModel> = collectionRepository.getCollection(maker)
+                if (collectionItemList.isEmpty()) {
+                    collectionItemList = listOf(EmptyStateItem(R.string.txt_empty_state_collection_item_list))
+                }
+                collectionItems.postValue(Result.Success(collectionItemList))
             } catch (e: Exception) {
                 println(e)
-                collectionItems.postValue(Result(e))
-            } finally {
-                loader.postValue(false)
+                collectionItems.postValue(Result.Error(throwable = e))
             }
         }
     }
